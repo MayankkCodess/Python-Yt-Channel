21a454528341d5d26e60fd4a54b5953626de7278
Mayank Kaushik <kaushmayank218@gmail.com>
2026-03-19 18:19:59 +0530

    if-else Topic python

---
+++ IfElse/Code.py
@@ -0,0 +1,20 @@
+age = 15
+
+# 1. Simple 'if'
+if age < 18:
+    print("You are a minor.")
+
+# 2. 'if-else'
+if age >= 18:
+    print("You can drive.")
+else:
+    print("You cannot drive yet.")
+
+# 3. 'if-elif-else'
+marks = 85
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+else:
+    print("Grade: C or below")
